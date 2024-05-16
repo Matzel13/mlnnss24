3347a7fc1a866669504083c6eaa5c64cbd802678
kjuen <klaus.juenemann@haw-hamburg.de>
2024-05-16 13:21:09 +0200

    Ergänzung Material Kap 5

--- 5_Gradientenabstieg/ADbeispiele.m
+++ 5_Gradientenabstieg/ADbeispiele.m
@@ -12,17 +12,17 @@ adsimple(f, [3;2])
 
 %% Regressionsbeispiel
 % Daten erzeugen
-N = 100; 
-x = rand(N, 1); 
-btrue = 0.4; 
-wtrue = -1.5; 
+N = 100;
+x = rand(N, 1);
+btrue = 0.4;
+wtrue = -1.5;
 y = btrue + wtrue*x + 0.2 * randn(N,1);
-D = [x, ones(N,1)]; 
-wbng = linsolve(D'*D, D'*y); 
-fD = figure('WindowStyle', 'docked'); 
-scatter(x,y, 'filled', 'DisplayName', 'Daten'); 
+D = [x, ones(N,1)];
+wbng = linsolve(D'*D, D'*y);
+fD = figure('WindowStyle', 'docked');
+scatter(x,y, 'filled', 'DisplayName', 'Daten');
 hold on;
-plot(x, wbng(2) + wbng(1)*x, 'DisplayName', 'Fit (Norm-Gl.)'); 
+plot(x, wbng(2) + wbng(1)*x, 'DisplayName', 'Fit (Norm-Gl.)');
 hold off;
 legend('Location','NE');
 
@@ -31,23 +31,40 @@ legend('Location','NE');
 costFunc = @(wb) MSEgerade(x, y, wb);
 gradFunc = @(wb) adsimple(costFunc, wb);
 eta = 0.8;
-nIts = 160; 
-wb0 = [1;1];   
-wbopt = gaEinfach(gradFunc, eta, wb0, nIts);
-
-hold on; 
-plot(x, wbopt(2) + wbopt(1)*x, ...
-   'DisplayName', sprintf('Fit (Grad. Abst., #Its: %i)', nIts));
+nIts = 60;
+wb0 = [1;1];
+[wbopt, track] = gaEinfach(gradFunc, eta, wb0, nIts);
+hold on;
+plot(x, wbopt(1)*x + wbopt(2), ...
+    'DisplayName', sprintf('Fit (Grad. Abst., #Its: %i)', nIts));
 hold off;
-xlabel('x'), ylabel('y'); 
+xlabel('x'), ylabel('y');
 title('Regressionsgerade mit Gradientenabstieg')
+
+
+%% Animation der GA-Optimierung und Plot der Kostenfunktion
+costVec = arrayfun(@(i) costFunc(track(:,i)), 1:nIts);
+for i =1:nIts
+    w = track(1, i);
+    b = track(2, i);
+    tiledlayout(2,1);
+    nexttile
+    scatter(x,y, 'filled');
+    hold on, plot(x, w*x+b), hold off;
+    nexttile;
+    plot(1:nIts, costVec);
+    hold on, scatter(i, costVec(i), 'r', 'filled'), hold off;
+    xlabel('Iteration'), ylabel('Kostenfunktion');
+    pause(0.2);
+
+end
 
 
 %% Beispiel fuer Matlabs dl-Framework
 f = @(x) 2*x^3;
 dlGrad(f, 2)
 
-adsimple(@newtonSqrt, 2)
+dlGrad(@newtonSqrt, 2)
 
 %% Zwei Variable
 f = @(w) w(1)^2*w(2);
@@ -56,8 +73,8 @@ dlGrad(f, [3;2])
 %% Nochmal das Regressionsbeispiel, jetzt mit dem fl-Framework
 
 % Modell definieren
-layers = [featureInputLayer(1), 
-   fullyConnectedLayer(1)]; 
+layers = [featureInputLayer(1),
+    fullyConnectedLayer(1)];
 mdl = dlnetwork(layers);
 summary(mdl)
 % analyzeNetwork(mdl)
@@ -70,18 +87,18 @@ dly = dlarray(y, 'BC');
 
 eta = 0.1;
 vel = [];    % Speichern des momentums
-nEpochs = 100; 
+nEpochs = 100;
 for i = 1:nEpochs
-   [loss, grad] = dlfeval(@dlMSE, mdl, dlx, dly);
-   fprintf('loss = %f\n', loss);
-   % Stoch. GA mit momentum
-   [mdl,vel] = sgdmupdate(mdl, grad, vel, eta);
+    [loss, grad] = dlfeval(@dlMSE, mdl, dlx, dly);
+    fprintf('loss = %f\n', loss);
+    % Stoch. GA mit momentum
+    [mdl,vel] = sgdmupdate(mdl, grad, vel, eta);
 end
 % Gefundene Parameter
-wbml = [extractdata(mdl.Learnables.Value{1}), extractdata(mdl.Learnables.Value{2})]; 
+wbml = [extractdata(mdl.Learnables.Value{1}), extractdata(mdl.Learnables.Value{2})];
 % Anwenden des Modells
 ypredml = extractdata(forward(mdl, dlx));
-hold on; 
+hold on;
 plot(x, ypredml, 'DisplayName', 'ML-Framework');
 hold off;
 
@@ -90,15 +107,15 @@ hold off;
 mdl2 = [layers; ...
     regressionLayer];   % impliziert MSE-Kostenfunktion
 options = trainingOptions('sgdm',...
-   'MaxEpochs', nEpochs, ...
-   'MinibatchSize', N, ...
-   'InitialLearnRate', eta, ...
-   'Verbose', true, ...
-   'Plots', 'None'); 
+    'MaxEpochs', nEpochs, ...
+    'MinibatchSize', N, ...
+    'InitialLearnRate', eta, ...
+    'Verbose', true, ...
+    'Plots', 'None');
 [net, info] = trainNetwork(x, y, mdl2, options);
 
 % Anwenden des Modells
 ypredml2 = predict(net, x);
-hold on; 
+hold on;
 plot(x, ypredml2, DisplayName="ML-Framework (V2)");
 hold off;
